9e3a57942bbaada5d018db600c5d732d7b48aa5b
Eden Yoon <yolha79@gmail.com>
2016-02-17 01:10:49 +0900

    add a function force to use speaker for media and cancel

--- src/com/humanebyte/lifelogging/MyActivity.kt
+++ src/com/humanebyte/lifelogging/MyActivity.kt
@@ -11,6 +11,14 @@ import android.widget.TextView
 class MyActivity : Activity(), View.OnClickListener {
     internal lateinit var service_status: TextView
     internal lateinit var setting: Button
+    internal lateinit var button_force_speaker: Button
+    internal lateinit var button_cancel_force_use: Button
+
+    internal val FOR_MEDIA = 1
+    internal val FORCE_NONE = 0
+    internal val FORCE_SPEAKER = 1
+
+    internal val setForceUse = Class.forName("android.media.AudioSystem").getMethod("setForceUse", Integer.TYPE, Integer.TYPE)
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,10 @@ class MyActivity : Activity(), View.OnClickListener {
         service_status = findViewById(R.id.accessibility_service_status) as TextView
         setting = findViewById(R.id.setting) as Button
         setting.setOnClickListener(this)
+        button_force_speaker = findViewById(R.id.force_speaker) as Button
+        button_force_speaker.setOnClickListener(this)
+        button_cancel_force_use = findViewById(R.id.cancel_force_use) as Button
+        button_cancel_force_use.setOnClickListener(this)
     }
 
     override fun onResume() {
@@ -41,6 +53,20 @@ class MyActivity : Activity(), View.OnClickListener {
             R.id.setting -> {
                 val accessibilityServiceIntent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
                 startActivity(accessibilityServiceIntent)
+            }
+            R.id.force_speaker -> {
+                try {
+                    setForceUse.invoke(null, FOR_MEDIA, FORCE_SPEAKER)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+            R.id.cancel_force_use -> {
+                try {
+                    setForceUse.invoke(null, FOR_MEDIA, FORCE_NONE)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
             }
         }
     }

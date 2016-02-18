7212b0bf6ffb0c83d2af6639fb132b6aab4d90f9
Eden Yoon <yolha79@gmail.com>
2016-02-19 01:08:11 +0900

    remove a useless line

--- src/com/humanebyte/lifelogging/MyAccessibilityService.kt
+++ src/com/humanebyte/lifelogging/MyAccessibilityService.kt
@@ -4,8 +4,6 @@ package com.humanebyte.lifelogging
  * Created by edenyoon on 2016. 2. 16..
  */
 
-import com.humanebyte.lifelogging.AccessibilityEventCaptureService
-
 class MyAccessibilityService : AccessibilityEventCaptureService() {
     init {
         this.setTag(MyAccessibilityService::class.java.simpleName)
